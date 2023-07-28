f0c5b04c281c2e8d8f8048d536e2a3b8ea401552
Jusy Lopes <jusylopes@outlook.com>
2023-07-27 22:04:18 -0300

    add: chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gpt_chatbot/utils/assets_manager.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2,
+        leading: Image.asset(AssetsManager.openaiLogo),
+      ),
+    );
   }
 }
