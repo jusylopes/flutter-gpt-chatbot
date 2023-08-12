9681d7b201704cd3b5b009326fb262bbe7cd2bfc
Jusy Lopes <jusylopes@outlook.com>
2023-08-11 22:00:24 -0300

    add: provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gpt_chatbot/providers/chat_provider.dart';
+import 'package:flutter_gpt_chatbot/repositories/chat_repository.dart';
 import 'package:flutter_gpt_chatbot/screens/chat_screen.dart';
 import 'package:flutter_gpt_chatbot/utils/colors.dart';
 import 'package:flutter_gpt_chatbot/utils/constants.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,15 +16,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: Constants.appName,
-      theme: ThemeData(
-        scaffoldBackgroundColor: GptColors.backgroundColor,
-        appBarTheme: const AppBarTheme(color: GptColors.primaryColor),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ChatProvider(
+            repository: ChatRepository(dio: Dio()),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: Constants.appName,
+        theme: ThemeData(
+          scaffoldBackgroundColor: GptColors.backgroundColor,
+          appBarTheme: const AppBarTheme(color: GptColors.primaryColor),
+          useMaterial3: true,
+        ),
+        home: const ChatScreen(),
       ),
-      home: const ChatScreen(),
     );
   }
 }
---
+++ lib/providers/chat_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gpt_chatbot/models/chat_model.dart';
+import 'package:flutter_gpt_chatbot/repositories/chat_repository.dart';
+
+class ChatProvider with ChangeNotifier {
+  final ChatRepository _repository;
+
+  ChatProvider({required ChatRepository repository}) : _repository = repository;
+
+  final List<ChatModel> _chatMessages = [];
+  List<ChatModel> get chatMessages => _chatMessages;
+
+  void addUserMsg({required String message}) {
+    _chatMessages.add(
+        ChatModel(message: message, chatMessageType: ChatMessageType.user));
+    notifyListeners();
+  }
+
+  Future<void> addBotMessage({required String message}) async {
+    try {
+      String botResponse =
+          await _repository.getMessageFromChatGPT(message: message);
+
+      _chatMessages.add(ChatModel(
+          message: botResponse, chatMessageType: ChatMessageType.bot));
+      notifyListeners();
+    } catch (error) {
+      print('Error sending message: $error');
+    }
+  }
+}
